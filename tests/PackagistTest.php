87817fa31fe7090af7670dad39cb17d57ee9ebe0
siad007 <siad.ardroumli@gmail.com>
2013-06-16 19:48:24 +0200

    init module

---
+++ tests/PackagistTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Zend\View\Helper;
+
+class AbstractHelper
+{
+
+}
+require_once __DIR__ . '/../Module.php';
+
+class PackagistTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     *
+     */
+    public function testSearchNoResult()
+    {
+        $packagist = new \ViewHelper_Packagist\Module();
+
+        $list = $packagist->search('qwertyqwertz');
+
+        $this->assertEquals('<ul id="packagistList"><li class="no-result">No result.</li></ul>', $list);
+    }
+
+}

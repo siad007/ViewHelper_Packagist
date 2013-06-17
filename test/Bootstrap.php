0c66547ba9ff55889bb77e6cb31b451691a05d7b
Siad Ardroumli <siad.ardroumli@googlemail.com>
2013-06-17 19:02:48 +0200

    Update Bootstrap.php

--- test/Bootstrap.php
+++ test/Bootstrap.php
@@ -8,7 +8,7 @@ use Zend\ServiceManager\ServiceManager;
 use Zend\Stdlib\ArrayUtils;
 use RuntimeException;
 
-$loader = include 'vendor/autoload.php';
+$loader = include '../vendor/autoload.php';
 
 if (!class_exists('Zend\Loader\AutoloaderFactory')) {
     throw new RuntimeException('Unable to load ZF2. Run `php composer.phar install --dev`');

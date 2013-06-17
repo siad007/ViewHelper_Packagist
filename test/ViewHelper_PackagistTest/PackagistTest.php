669b5aa1f743b0fa0a4bd3785b175dea84579276
siad007 <siad.ardroumli@gmail.com>
2013-06-17 16:08:34 +0200

    test

---
+++ test/ViewHelper_PackagistTest/PackagistTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace ViewHelper_PackagistTest;
+
+use \PHPUnit_Framework_TestCase as TestCase;
+use ViewHelper_Packagist\View\Helper\Packagist;
+
+class PackagistTest extends TestCase
+{
+    /**
+     *
+     */
+    public function testSearchNoResult()
+    {
+        $packagist = new Packagist();
+
+        $list = $packagist->search('qwertyqwertz');
+
+        $this->assertEquals('<ul id="packagistList"><li class="no-result">No result.</li></ul>', $list);
+    }
+
+}

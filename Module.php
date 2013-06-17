d2abaf7c32840b7ea6f9e9dafdc90ecfc71db19f
Siad Ardroumli <siad.ardroumli@googlemail.com>
2013-06-17 14:49:13 +0200

    Update Module.php

--- Module.php
+++ Module.php
@@ -4,11 +4,9 @@ namespace ViewHelper_Packagist;
 
 class Module
 {
-    
     public function getAutoloaderConfig()
     {
         return array(
-            'Zend\Loader\ClassMapAutoloader' => array(__DIR__ . '/autoload_classmap.php'),
             'Zend\Loader\StandardAutoloader' => array(
                 'namespaces' => array(
                     __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,

7320d695ee6746ff98b228d3cff4e7d1faa089cf
Siad Ardroumli <siad.ardroumli@googlemail.com>
2013-06-17 15:04:40 +0200

    Update bootstrap.php

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,5 +1,28 @@
 <?php
 
+$path = array(
+    ZF2_PATH,
+    get_include_path(),
+);
+set_include_path(implode(PATH_SEPARATOR, $path));
+
+require_once 'Zend/Loader/AutoloaderFactory.php';
+require_once 'Zend/Loader/StandardAutoloader.php';
+
+use Zend\Loader\AutoloaderFactory;
+use Zend\Loader\StandardAutoloader;
+
+// setup autoloader
+AutoloaderFactory::factory(
+    array(
+     'Zend\Loader\StandardAutoloader' => array(
+            StandardAutoloader::AUTOREGISTER_ZF => true,
+            StandardAutoloader::ACT_AS_FALLBACK => false,
+            StandardAutoloader::LOAD_NS => $additionalNamespaces,
+        )
+    )
+);
+
 $files = array(
     __DIR__ . '/../vendor/autoload.php',
     __DIR__ . '/../../../vendor/autoload.php',

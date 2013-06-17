b86590870ab15629a815ac8c4f1e92a01f6ca4cd
Siad Ardroumli <siad.ardroumli@googlemail.com>
2013-06-17 21:57:42 +0200

    Update PackagistTest.php

--- test/ViewHelper_PackagistTest/PackagistTest.php
+++ test/ViewHelper_PackagistTest/PackagistTest.php
@@ -9,15 +9,43 @@ use Zend\Http\Client;
 class PackagistTest extends TestCase
 {
     /**
-     *
+     * @var Zend\Http\Client
      */
-    public function testSearchNoResult()
+    protected $client;
+    protected $adapter;
+    
+    public function setUp()
+    {
+        $this->client = new Client('http://fake');
+        $this->adapter = new \Zend_Http_Client_Adapter_Test();
+        $this->client->getHttpClient()->setAdapter($this->adapter);
+    }
+    
+    /**
+     * @test
+     */
+    public function searchNoResult()
     {
         $packagist = new Packagist();
-        $packagist->setHttpClient(new Client(null, array('adapter' => 'Zend\Http\Client\Adapter\Test')));
+        $packagist->setHttpClient($this->client);
         $list = $packagist->search(array('q' => 'qwertyqwertz'));
 
         $this->assertEquals('<ul id="packagistList"><li class="no-result">No result.</li></ul>', $list);
     }
-
+    
+    public function testCallWebServiceWhenSuccessful()
+    {
+        $this->adapter->setResponse(
+            "HTTP/1.1 200 OK" . "\r\n" .
+            "Content-Type: text/html" . "\r\n" .
+            "\r\n" .
+            'ok'
+        );
+        
+        $packagist = new Packagist();
+        $packagist->setHttpClient($this->client);
+        $list = $packagist->search(array('q' => 'qwertyqwertz'));
+        
+        $this->assertEquals('ok', $list);
+    }
 }
